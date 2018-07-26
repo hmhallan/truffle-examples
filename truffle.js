4e3cea2499b8b8acd96f4e9eb2f2e0757e325878
Hallan Medeiros <hmhallan@gmail.com>
2018-07-25 21:12:20 -0300

    ajustes no na config de testes

--- truffle.js
+++ truffle.js
@@ -4,19 +4,29 @@ module.exports = {
   
   
   networks: {
+    
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*", // Match any network id
-      gas: 2000000,   // <--- Twice as much
-      gasPrice: 10000000000,
+      gas: 4712388,   // <--- Twice as much
+      gasPrice: 36000000
+    },
+    ganache: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*", // Match any network id
+      //gas: 4712388,   // <--- Twice as much
+      //gasPrice: 36000000
     }
+    
   },
 
 
 
   mocha: {
     useColors: true,
+    /*
     reporter: 'eth-gas-reporter',
     reporterOptions : {
       currency: 'USD',
@@ -24,6 +34,9 @@ module.exports = {
       showTimeSpent: true,
       onlyCalledMethods: true
     }
-  }
+    */
+  },
+
+  //solc: { optimizer: { enabled: true, runs: 200 } }
   
 };
